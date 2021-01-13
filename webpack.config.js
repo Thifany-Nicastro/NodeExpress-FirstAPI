31066db182530379dd73e9a1be759237ab065ef5
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-01-13 15:22:27 -0300

    Hell

--- webpack.config.js
+++ webpack.config.js
@@ -6,18 +6,36 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'app.bundle.js'
     },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        publicPath: '/assets/'
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          }
+        ]
+      },
     resolve: {
-      fallback: {
-        stream: false,
-        util: false,
-        path: false,
-        url: false,
-        querystring: false,
-        fs: false,
-        http: false,
-        net: false,
-        crypto: false,
-        zlib: false
-      }
+        fallback: {
+            stream: false,
+            util: false,
+            path: false,
+            url: false,
+            querystring: false,
+            fs: false,
+            http: false,
+            net: false,
+            crypto: false,
+            zlib: false
+        }
     },
 };
