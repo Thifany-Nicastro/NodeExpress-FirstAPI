538fbfb608bdda0780c5b18180cbeb0385cb1286
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-01-15 11:38:49 -0300

    Separating index from app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
-import express from 'express';
 import 'dotenv/config';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import routes from './routes';
+import app from './app';
 
-const app = express()
 const port = process.env.NODE_PORT
-
-app.use(bodyParser.json());
-
-app.use('/api', routes);
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
